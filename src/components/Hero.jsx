1ea3a7d2d4a7f3115fdb67867e4b2f8891e41504
Womp Rat <devinmenzies@gmail.com>
2023-01-26 14:51:39 -0700

    hero-responsive at xl

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <h2>Hi! I'm</h2>
       <h1>Devin Menzies</h1>
       <Avatar img={pfp}/> 
-      <h2>I'm a web developer seeking opportunities for growth.</h2>
+      <h2>Web developer</h2>
     </div>
   )
 }
