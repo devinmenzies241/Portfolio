52d431dd29b1f9baad3aed54cf1887bb47d78189
Womp Rat <devinmenzies@gmail.com>
2023-01-31 08:33:13 -0700

    closing out change-box-shadows

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,14 +4,16 @@ const ProjectCard = (props) => {
   return (
     <div className='card project-card'>
       <a href={props.href} target='blank'>
-        <img src={props.imgSrc} className='car-img-top' alt='portfolio project thumbnail'></img>
-      </a>
-      <div className='card-body'>
-        <a href={props.href} target='blank'>
-          <h2>{props.name}</h2>  
-        </a> 
-        <p>{props.description}</p>
-      </div>
+        <div>
+          <img src={props.imgSrc} className='car-img-top' alt='portfolio project thumbnail'></img>
+          <div className='card-body'>
+            <a href={props.href} target='blank'>
+              <h2>{props.name}</h2>  
+            </a> 
+            <p>{props.description}</p>
+          </div>
+        </div>
+      </a>  
     </div>
   )
 }
