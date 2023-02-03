60b74089df556e539eabb8bddca676ec626de93d
Womp Rat <devinmenzies@gmail.com>
2023-02-02 17:55:57 -0700

    fix <a> target= 'blank' to '_blank'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
       <a 
         className='footer-link' 
         href='mailto:devinmenzies@gmail.com' 
-        target='blank'
+        target='_blank'
         onMouseEnter={()=> setHover1(true)} 
         onMouseLeave={()=> setHover1(false)}>
         <i className={`fa-regular fa-envelope ${hover1 ? 'fa-bounce' : '' }`}></i>
@@ -33,7 +33,7 @@ const Footer = () => {
       <a 
         className='footer-link' 
         href='https://docs.google.com/document/d/e/2PACX-1vQpYfn9y0OXR81iPTQllsZqNtI6qqIZobnlNpB5bhYGw0DhHaCbAvK38HgW7OcsU8O4brJNo-NgF__k/pub' 
-        target='blank'
+        target='_blank'
         onMouseEnter={()=> setHover2(true)} 
         onMouseLeave={()=> setHover2(false)}>
         <i className={`fa-regular fa-file ${hover2 ? 'fa-bounce' : '' }`}></i>
@@ -41,7 +41,7 @@ const Footer = () => {
       <a 
         className='footer-link' 
         href='https://www.linkedin.com/in/devin-menzies-01893671/' 
-        target='blank'
+        target='_blank'
         onMouseEnter={()=> setHover3(true)} 
         onMouseLeave={()=> setHover3(false)}>
         <i className={`fa-brands fa-linkedin ${hover3 ? 'fa-bounce' : '' }`}></i>
@@ -49,7 +49,7 @@ const Footer = () => {
       <a 
         className='footer-link' 
         href='https://github.com/devinmenzies241' 
-        target='blank'
+        target='_blank'
         onMouseEnter={()=> setHover4(true)} 
         onMouseLeave={()=> setHover4(false)}>
         <i className={`fa-brands fa-github ${hover4 ? 'fa-bounce' : '' }`}></i></a>
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const ProjectCard = (props) => {
   return (
     <div className='card project-card'>
-      <a href={props.href} target='blank'>
+      <a href={props.href} target='_blank'>
         <div>
           <img src={props.imgSrc} className='car-img-top' alt='portfolio project thumbnail'></img>
           <div className='card-body'>
-            <a href={props.href} target='blank'>
+            <a href={props.href} target='_blank'>
               <h2>{props.name}</h2>  
             </a> 
             <p>{props.description}</p>
